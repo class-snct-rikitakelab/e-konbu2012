c2429c6f6ed71135ae10ea5aaa37e0ff2b7e5111
Hiromitsu Homma <hitsk26@gmail.com>
2012-05-18 15:32:40 +0900

    sort

--- Sprint#2/tyreal/kernel_cfg.c
+++ Sprint#2/tyreal/kernel_cfg.c
@@ -1,6 +1,6 @@
 /*
  *  kernel_cfg.c
- *  Thu May 17 10:48:08 2012
+ *  Fri May 18 07:15:36 2012
  *  SG Version 2.00
  *  sg.exe sample.oil -os=ECC2 -IC:/cygwin/nxtOSEK/toppers_osek/sg/impl_oil -template=C:/cygwin/nxtOSEK/toppers_osek/sg/lego_nxt.sgt
  */
