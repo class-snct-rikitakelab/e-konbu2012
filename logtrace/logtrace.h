9972445d932ee4c4f0e5858db6369ad2dbe898b7
YutaNiimura <lastarpeggio@gmail.com>
2012-05-25 08:42:58 +0900

    Nmura add

--- logtrace/logtrace.h
+++ logtrace/logtrace.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _SAMPLE10_H_
 #define _SAMPLE10_H_
 
@@ -6,20 +5,6 @@
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
 #include "balancer.h"
-//#include "TimerUnit.h"
 #include "math.h"
 
 #endif
-=======
-#ifndef _SAMPLE10_H_
-#define _SAMPLE10_H_
-
-#include "kernel.h"
-#include "kernel_id.h"
-#include "ecrobot_interface.h"
-#include "balancer.h"
-//#include "TimerUnit.h"
-#include "math.h"
-
-#endif
->>>>>>> 78ef82cfe7bdee3aec871e7d5647b625f76d9c3a
