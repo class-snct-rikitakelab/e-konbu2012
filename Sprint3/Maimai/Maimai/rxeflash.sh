9e094ec022546fa6c219e587ea997f2ba634d4f8
N_MURA <N_MURA@Saturn.(none)>
2012-05-31 16:49:08 +0900

    vs add

---
+++ Sprint3/Maimai/Maimai/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload sample_c4.rxe...
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=sample_c4.rxe
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=sample_c4.rxe
+echo NeXTTool is terminated.
