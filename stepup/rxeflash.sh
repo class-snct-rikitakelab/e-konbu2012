d6665f023de35218874ffdb8f8af93c7c2813d9a
Niimura Yuta <lastarpeggio@gmail.com>
2012-06-19 11:56:31 +0900

    logtrace and e-konbu splited

--- stepup/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload stepup.rxe...
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=stepup.rxe
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=stepup.rxe
-echo NeXTTool is terminated.
