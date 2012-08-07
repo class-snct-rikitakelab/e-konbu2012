1cfcd49d55bc1826f91c0997919a8f4e0183c960
hiromitsu <hitsk26@gmail.com>
2012-08-07 15:50:39 +0900

    a

--- RadioControl/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload RadioControl.rxe...
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=RadioControl.rxe
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=RadioControl.rxe
-echo NeXTTool is terminated.
--- markerDetection/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload markerdetection.rxe...
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=markerdetection.rxe
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=markerdetection.rxe
-echo NeXTTool is terminated.
