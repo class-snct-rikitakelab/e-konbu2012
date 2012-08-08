2caa409f2bd1eed95579d2cac96008ff6e0e9606
s1000514 <s1000514@sendai-nct.jp>
2012-08-08 09:47:30 +0900

    push

--- sample_PID/Kaidan.c
+++ sample_PID/Kaidan.c
@@ -40,9 +40,9 @@ static float bf_hensa = 0;
 
 //ライントレース時PID制御用係数
 
-static float Kp = 1.0944;			//P制御用
-static float Ki = 2.2;				//I制御用
-static float Kd = 0.002;				//D制御用
+static float Kp = 1.360;			//P制御用
+static float Ki = 2.6;				//I制御用
+static float Kd = 0.003;				//D制御用
 
 
 static int wait_count = 0;
@@ -352,7 +352,7 @@ void RN_setting()
 
 			//通常走行
 		case (RN_RUN):
-			RA_linetrace_PID(80);
+			RA_linetrace_PID(100);
 			break;
 
 		default:
