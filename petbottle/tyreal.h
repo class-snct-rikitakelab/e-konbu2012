504003844fc24460d1dff64bd7fc27d16d5694f6
s1000514 <s1000514@sendai-nct.jp>
2012-08-29 12:25:38 +0900

    petbottle project

---
+++ petbottle/tyreal.h
@@ -0,0 +1,11 @@
+#define VOL 20
+#define SET_PARAM_SPEED 100
+#define INT_EXPRESS_RENGE 11
+#define FLOAT_EXPRESS_RENGE 5
+#define TYRE_ANGLE_CHAR_NUM 8
+#define ADJUST_INT_STEP 1
+#define ADJUST_FLOAT_STEP 1
+
+
+extern void do_tyreal();
+extern int tyreal_trigger();
