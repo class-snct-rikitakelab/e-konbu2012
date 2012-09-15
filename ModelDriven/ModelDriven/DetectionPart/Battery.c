27b5c70e4de52e63080bdcd04d022cc34f9cba61
Niimura Yuta <lastarpeggio@gmail.com>
2012-09-15 16:30:16 +0900

    battery modified

--- ModelDriven/ModelDriven/DetectionPart/Battery.c
+++ ModelDriven/ModelDriven/DetectionPart/Battery.c
@@ -7,9 +7,7 @@ void Battery_init(Battery *this_Battery){
 BOOL Battery_dectRedct(Battery *this_Battery){
 	BOOL res=false;
 
-	/*
-	if(this_Battery->initVol   )
-	i don't know how to implement. plase 
-	*/
-
+	if(this_Battery->initVol - ecrobot_get_battery_voltage() > this_Battery->targReductRang)
+		res = true;
 }
+
