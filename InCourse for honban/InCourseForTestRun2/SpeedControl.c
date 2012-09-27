07e9ffe01f54903b51a4f02fe954f963bd31e14a
hiromitsu <hitsk26@gmail.com>
2012-09-27 15:40:22 +0900

    changed foldere name form incorse to in corse for honban

---
+++ InCourse for honban/InCourseForTestRun2/SpeedControl.c
@@ -0,0 +1,30 @@
+#include "SpeedControl.h"
+#include "runnerForTestRun.h"
+
+//段階的加速用関数（指定量だけ速度を徐々に上昇）
+int RA_speed(int forward_speed){
+
+	static int result_speed = /*TARGET_SPEED*/0;
+	static int counter = 0;
+	
+	counter++;
+
+	if(counter >= SPEED_COUNT)
+	{
+		if(forward_speed - result_speed >= 0){
+			result_speed++;
+
+			if(result_speed > forward_speed)
+				result_speed = forward_speed;
+		}
+		else{
+			result_speed--;
+
+			if(result_speed < forward_speed)
+				result_speed = forward_speed;
+		}
+		counter = 0;
+	}
+
+	return result_speed;
+}
