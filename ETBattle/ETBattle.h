32d0852b75d23edd1f2edbb158001015c62685ea
Niimura Yuta <lastarpeggio@gmail.com>
2012-10-04 15:03:01 +0900

    ETBattle program renewed

---
+++ ETBattle/ETBattle.h
@@ -0,0 +1,12 @@
+#ifndef _ETBATTLE_H_
+#define _ETBATTLE_H_
+
+#include "TailControl.h"
+
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+#include "balancer.h"
+#include "math.h"
+
+#endif
---
+++ ETBattle/TailControl.h
@@ -0,0 +1,29 @@
+#ifndef _TAILCONTROL_H_
+#define _TAILCONTROL_H_
+
+#include "kernel.h"
+#include "kernel_id.h"
+#include "ecrobot_interface.h"
+#include "balancer.h"
+#include "math.h"
+
+#define ANGLEOFDOWN		105	//尻尾走行
+#define ANGLEOFZERO		0
+#define ANGLEOFUP		6	//尻尾不使用
+#define ANGLEOFLOOKUP	56	//ルックアップゲート通過
+#define ANGLEOFSTAND	100	//直立状態
+#define ANGLEOFPUSH		120	//走行体跳ね上げ
+
+#define TAIL_ANGLE_COUNT 1
+
+//尻尾PI制御用係数
+static float t_Kp = 5.85;			//P制御用
+
+static int result_angle = 0;
+static int target_angle = ANGLEOFZERO;
+
+extern void TailControl();
+extern void TargetTailAngleControl();
+extern void TailAngleChange(int angle);
+
+#endif
